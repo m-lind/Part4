3cd46c55123d8252b652d2595c4baa4c2c74d900
m-lind <mari.j.lindstedt@gmail.com>
2023-06-16 13:42:00 +0300

    Added functionality to remove a single blog from list and a test to make sure the deletion works correctly

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -84,6 +84,19 @@ test("400 Bad request if url is missing", async () => {
   await api.post("/api/blogs").send(newBlog).expect(400);
 });
 
+test("a blog can be deleted", async () => {
+  const blogAtStart = await helper.blogsInDb();
+  const blogToDelete = blogAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+  const blogs = blogsAtEnd.map(b => b.title);
+  expect(blogs).not.toContain(blogToDelete.title);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
