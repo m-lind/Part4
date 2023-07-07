2efdcfaa9e176141070b8a265fe024d1ae4c6f6a
m-lind <mari.j.lindstedt@gmail.com>
2023-07-07 15:50:16 +0300

    One of the users is defined as the blog creator

--- controllers/users.js
+++ controllers/users.js
@@ -25,7 +25,12 @@ usersRouter.post("/", async (request, response) => {
 });
 
 usersRouter.get("/", async (reequest, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
   response.json(users);
 });
 
