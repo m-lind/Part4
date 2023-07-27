cc6de6878aa80801ca6febb40efa0ccc9e6428f4
m-lind <mari.j.lindstedt@gmail.com>
2023-07-27 17:48:13 +0300

    Fixed tests that didn't work with token based authorization, added a test makes sure a valid token is provided when a blog entry is added, refactored tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,62 +2,75 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  const body = request.body;
-  const user = request.user;
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id,
-  });
-
-  const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
-
-  response.status(201).json(savedBlog);
-});
-
-blogsRouter.delete("/:id", async (request, response, next) => {
-  const blog = await Blog.findById(request.params.id);
-  if (!blog) {
-    return response.status(404).json({ error: "blog not found" });
-  }
-
-  const user = request.user;
-  if (blog.user.toString() === user.id.toString()) {
-    await Blog.findByIdAndRemove(request.params.id);
-    response.status(204).end();
-  } else {
-    response.status(401).json({
-      error: "invalid username",
+blogsRouter.post(
+  "/",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const body = request.body;
+    const user = request.user;
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
     });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.status(201).json(savedBlog);
   }
-});
+);
 
-blogsRouter.put("/:id", async (request, response, next) => {
-  const body = request.body;
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const blog = await Blog.findById(request.params.id);
+    if (!blog) {
+      return response.status(404).json({ error: "blog not found" });
+    }
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  };
+    const user = request.user;
+    if (blog.user.toString() === user.id.toString()) {
+      await Blog.findByIdAndRemove(request.params.id);
+      response.status(204).end();
+    } else {
+      response.status(401).json({
+        error: "invalid username",
+      });
+    }
+  }
+);
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog);
-    })
-    .catch(error => next(error));
-});
+blogsRouter.put(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const body = request.body;
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    };
+
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog);
+      })
+      .catch(error => next(error));
+  }
+);
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,116 +8,207 @@ const Blog = require("../models/blog");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
+let authToken;
+
+beforeAll(async () => {
+  const loggedInUser = {
+    username: process.env.TEST_USERNAME,
+    password: process.env.TEST_PASSWORD,
+  };
+
+  await api
+    .post("/api/users")
+    .send(loggedInUser)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api
+    .post("/api/login")
+    .send(loggedInUser)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  authToken = response.body.token;
+});
+
 beforeEach(async () => {
   await Blog.deleteMany({});
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("returning a list of blogs", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("the amount of blogs is correct", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test("the amount of blogs is correct", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+describe("creating a new blog entry", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "I am a valid blog",
+      author: "Valid Valid",
+      url: "https://valid.valid",
+      likes: 344,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const authors = blogsAtEnd.map(b => b.author);
+    expect(authors).toContain("Valid Valid");
+  });
+
+  test("Blog cannot be added without a token", async () => {
+    const newBlog = {
+      title: "No token",
+      author: "No Token",
+      url: "https://no.token",
+    };
+    await api.post("/api/blogs").send(newBlog).expect(401);
+  });
+
+  test("The default number of likes is 0", async () => {
+    const newBlog = {
+      title: "No likes is defined",
+      author: "Blogger Blogger",
+      url: "https://blogger.blogger",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(201);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    blogWithNoLikes = blogsAtEnd.find(b => b.author === "Blogger Blogger");
+    expect(blogWithNoLikes.likes).toBe(0);
+  });
+
+  test("400 Bad request if title is missing", async () => {
+    const newBlog = {
+      author: "Blogger Blogger",
+      url: "https://blogger.blogger",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(400);
+  });
+
+  test("400 Bad request if url is missing", async () => {
+    const newBlog = {
+      title: "Blog without url",
+      author: "Blogger Blogger",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(400);
+  });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "I am a valid blog",
-    author: "Valid Valid",
-    url: "https://valid.valid",
-    likes: 344,
-  };
+describe("viewing and updating a blog entry", () => {
+  test("Individual blog id is named id", async () => {
+    const blogsAtEnd = await helper.blogsInDb();
+    blogsAtEnd.map(b => expect(b.id).toBeDefined());
+  });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+  test("a blog can be updated", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = {
+      title: "This blog will be updated",
+      author: "Updated Blog",
+      url: "https://blog.to.update",
+    };
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    await api
+      .post("/api/blogs")
+      .send(blogToUpdate)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(201);
 
-  const authors = blogsAtEnd.map(b => b.author);
-  expect(authors).toContain("Valid Valid");
+    const blogsWithBlogToUpdate = await helper.blogsInDb();
+    const blogToBeUpdated = blogsWithBlogToUpdate.find(
+      b => b.title === "This blog will be updated"
+    );
+
+    const updatedBlog = {
+      title: "I am an updated blog",
+      author: "Updated Updated",
+      url: "https://updated.updated",
+      likes: 5790,
+    };
+
+    await api
+      .put(`/api/blogs/${blogToBeUpdated.id}`)
+      .send(updatedBlog)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(200);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length + 1);
+    const updatedBlogInDb = blogsAtEnd.find(
+      blog => blog.id === blogToBeUpdated.id
+    );
+
+    expect(updatedBlog.title).toBe(updatedBlogInDb.title);
+    expect(updatedBlog.author).toBe(updatedBlogInDb.author);
+    expect(updatedBlog.url).toBe(updatedBlogInDb.url);
+    expect(updatedBlog.likes).toBe(updatedBlogInDb.likes);
+  });
 });
 
-test("Individual blog id is named id", async () => {
-  const blogsAtEnd = await helper.blogsInDb();
-  blogsAtEnd.map(b => expect(b.id).toBeDefined());
-  //console.log(ids);
-});
+describe("deletion of a blog entry", () => {
+  test("a blog can be deleted", async () => {
+    const blogToDelete = {
+      title: "This will be deleted",
+      author: "Delete Delete",
+      url: "delete.delete",
+    };
 
-test("The default number of likes is 0", async () => {
-  const newBlog = {
-    title: "No likes is defined",
-    author: "Blogger Blogger",
-    url: "https://blogger.blogger",
-  };
+    await api
+      .post("/api/blogs")
+      .send(blogToDelete)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(201);
 
-  await api.post("/api/blogs").send(newBlog).expect(201);
+    const blogsWithBlogToDelete = await helper.blogsInDb();
 
-  const blogsAtEnd = await helper.blogsInDb();
+    const blogToBeDeleted = blogsWithBlogToDelete.find(
+      b => b.title === "This will be deleted"
+    );
 
-  blogWithNoLikes = blogsAtEnd.find(b => b.author === "Blogger Blogger");
-  //console.log(blogWithNoLikes);
-  expect(blogWithNoLikes.likes).toBe(0);
-});
+    await api
+      .delete(`/api/blogs/${blogToBeDeleted.id}`)
+      .set("Authorization", `Bearer ${authToken}`)
+      .expect(204);
 
-test("400 Bad request if title is missing", async () => {
-  const newBlog = {
-    author: "Blogger Blogger",
-    url: "https://blogger.blogger",
-  };
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-});
-
-test("400 Bad request if url is missing", async () => {
-  const newBlog = {
-    title: "Blog without url",
-    author: "Blogger Blogger",
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-});
-
-test("a blog can be deleted", async () => {
-  const blogAtStart = await helper.blogsInDb();
-  const blogToDelete = blogAtStart[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-  expect(blogsAtEnd).not.toContain(blogToDelete);
-});
-
-test("a blog can be updated", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToUpdate = blogsAtStart[0];
-  const updatedBlog = {
-    title: "I am an updated blog",
-    author: "Updated Updated",
-    url: "https://updated.updated",
-    likes: 5790,
-  };
-
-  await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(blogsAtStart.length);
-  const updatedBlogInDb = blogsAtEnd.find(blog => blog.id === blogToUpdate.id);
-
-  expect(updatedBlog.title).toBe(updatedBlogInDb.title);
-  expect(updatedBlog.author).toBe(updatedBlogInDb.author);
-  expect(updatedBlog.url).toBe(updatedBlogInDb.url);
-  expect(updatedBlog.likes).toBe(updatedBlogInDb.likes);
+    expect(blogsAtEnd).not.toContain(blogToBeDeleted);
+  });
 });
 
 describe("when there is initially one user at db", () => {
@@ -134,9 +225,9 @@ describe("when there is initially one user at db", () => {
     const userAtStart = await helper.usersInDb();
 
     const newUser = {
-      username: "nuser",
-      name: "New User",
-      password: "salainen",
+      username: process.env.CREATE_TEST_USERNAME,
+      name: process.env.CREATE_TEST_NAME,
+      password: process.env.CREATE_TEST_PASSWORD,
     };
 
     await api
